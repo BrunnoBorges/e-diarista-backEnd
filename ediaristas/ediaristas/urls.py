31482663323c7641c9ced11fa01151b257511d35
brunno.borges <brunno.borges@germinare.org.br>
2021-06-16 21:56:58 -0300

    ajustando caminho para renderizar imagem

--- ediaristas/ediaristas/urls.py
+++ ediaristas/ediaristas/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('web/', include('web.urls')),
     path('api/', include('api.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
